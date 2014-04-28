9781620436ff4d2dcde03cea05530766311984f8
Daniel Marantz <danielmarantz@msn.com>
2014-04-27 18:49:53 -0700

    I added one sample sprite button, but for some reason there is two. one of them vibrates if touched.

--- TopGun/src/com/bcit/project/topgun/UI_Menu.java
+++ TopGun/src/com/bcit/project/topgun/UI_Menu.java
@@ -1,6 +1,7 @@
 package com.bcit.project.topgun;
 
 import org.andengine.engine.camera.Camera;
+import org.andengine.engine.camera.hud.HUD;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
@@ -17,6 +18,10 @@ import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.texture.region.TiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
+import org.andengine.input.touch.TouchEvent;
+
+import android.content.Context;
+import android.os.Vibrator;
 
 public class UI_Menu extends SimpleBaseGameActivity {
 	// ===========================================================
@@ -39,6 +44,11 @@ public class UI_Menu extends SimpleBaseGameActivity {
 	private ITextureRegion mParallaxLayerBack;
 	private ITextureRegion mParallaxLayerMid;
 	private ITextureRegion mParallaxLayerFront;
+	private ITextureRegion hudImage;
+	
+	private HUD hud;
+	private Sprite hudSprite;
+	private Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);;
 
 	// ===========================================================
 	// Constructors
@@ -51,11 +61,10 @@ public class UI_Menu extends SimpleBaseGameActivity {
 	// ===========================================================
 	// Methods for/from SuperClass/Interfaces
 	// ===========================================================
-
+	
 	@Override
 	public EngineOptions onCreateEngineOptions() {
-		final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-
+		
 		return new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera);
 	}
 
@@ -67,18 +76,21 @@ public class UI_Menu extends SimpleBaseGameActivity {
 		this.mPlayerTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(this.mBitmapTextureAtlas, this, "player.png", 0, 0, 3, 4);
 		this.mEnemyTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(this.mBitmapTextureAtlas, this, "enemy.png", 73, 0, 3, 4);
 		this.mBitmapTextureAtlas.load();
-
+	
 		this.mAutoParallaxBackgroundTexture = new BitmapTextureAtlas(this.getTextureManager(), 1024, 1024);
 		this.mParallaxLayerFront = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mAutoParallaxBackgroundTexture, this, "parallax_background_layer_front.png", 0, 0);
 		this.mParallaxLayerBack = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mAutoParallaxBackgroundTexture, this, "parallax_background_layer_back.png", 0, 188);
 		this.mParallaxLayerMid = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mAutoParallaxBackgroundTexture, this, "parallax_background_layer_mid.png", 0, 669);
+		
+		this.hudImage=BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mAutoParallaxBackgroundTexture, this, "menu_quit.png", 150, 250);
+		
 		this.mAutoParallaxBackgroundTexture.load();
 	}
 
 	@Override
 	public Scene onCreateScene() {
 		this.mEngine.registerUpdateHandler(new FPSLogger());
-
+		
 		final Scene scene = new Scene();
 		final AutoParallaxBackground autoParallaxBackground = new AutoParallaxBackground(0, 0, 0, 5);
 		final VertexBufferObjectManager vertexBufferObjectManager = this.getVertexBufferObjectManager();
@@ -90,7 +102,7 @@ public class UI_Menu extends SimpleBaseGameActivity {
 		/* Calculate the coordinates for the face, so its centered on the camera. */
 		final float playerX = (CAMERA_WIDTH - this.mPlayerTextureRegion.getWidth()) / 2;
 		final float playerY = CAMERA_HEIGHT - this.mPlayerTextureRegion.getHeight() - 5;
-
+		
 		/* Create two sprits and add it to the scene. */
 		final AnimatedSprite player = new AnimatedSprite(playerX, playerY, this.mPlayerTextureRegion, vertexBufferObjectManager);
 		player.setScaleCenterY(this.mPlayerTextureRegion.getHeight());
@@ -101,10 +113,29 @@ public class UI_Menu extends SimpleBaseGameActivity {
 		enemy.setScaleCenterY(this.mEnemyTextureRegion.getHeight());
 		enemy.setScale(2);
 		enemy.animate(new long[]{200, 200, 200}, 3, 5, true);
-
+		
+		//Creating a vibrator
+		final Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+		
+		// Creating the sprite button
+		hudSprite = new Sprite(150, 250, this.hudImage, vertexBufferObjectManager)	{
+            @Override
+            public boolean onAreaTouched(final TouchEvent pAreaTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+            	
+            	vib.vibrate(50);	
+                return true;
+            }       
+		};
+	
 		scene.attachChild(player);
 		scene.attachChild(enemy);
-
+		
+		// Creating the HUD and attaching the Sprite button
+		hud = new HUD();
+		hud.attachChild(hudSprite);
+		hud.registerTouchArea(hudSprite);
+        camera.setHUD(hud);
+	     
 		return scene;
 	}
 
